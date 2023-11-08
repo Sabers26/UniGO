20d31016587df7a0940e95f3684f68c73580a368
LuisSaez <luissaez261002@gmail.com>
2023-11-08 15:41:33 -0300

    cambio de rama2

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -11,45 +11,6 @@ export class HomePage {
   constructor(private router:Router) {
     
   }
-<<<<<<< HEAD
   pedir(){
-    
-=======
-  viaje:Viaje={
-    direccion:'',
-    costo:0,
-    conductor:this.usuario,
-    auto:this.auto,
-    pasajeros:[],
-    estado:false
-  }
-  sesion:Sesion={
-    id:0,
-    usr:this.usuario,
-    aut:this.auto,
-    viaje:this.viaje
-  }
-  viajes:Viaje[]=[]
-  constructor(private router:Router, private authStorage:AutenticacionStorageService, private viajeFire:ViajeServicioService) {
-    this.authStorage.getSesion().then(item=>{
-      this.sesion.id=item.id
-      this.sesion.usr=item.usr
-    })
-    this.viajeFire.getAllViajes().then(item=>{
-      if(item!==undefined)
-      {
-        console.log(item)
-        this.viajes=item
-      }
-    })
-  }
-  pedir(conductor:Usuario){
-    this.viajeFire.aggPasajero(this.sesion.usr, conductor).then(item=>{
-      if(item)
-      {
-        this.router.navigate(['/tabs/viajes'])
-      }
-    })
->>>>>>> 0ebebce3c14779c049bf99e84cde962b808c10c1
   }
 }
